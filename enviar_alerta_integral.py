d6494fcd8b1542bdf8cc5c0c35cb343069310fd5
christianherbert1039-ui <christianherbert1039@gmail.com>
2026-09-24 11:59:55 +0100

    Update enviar_alerta_integral.py

--- enviar_alerta_integral.py
+++ enviar_alerta_integral.py
@@ -1,1 +1,27 @@
 
+Go to file
+t
+T
+christianherbert1039-ui
+christianherbert1039-ui
+Update enviar_alerta_integral.py
+71eaea4
+ · 
+17 hours ago
+Name		
+.github/workflows
+Update ejecutar.yml
+19 hours ago
+.gitignore
+Initial commit
+yesterday
+README.md
+Initial commit
+yesterday
+enviar_alerta_integral.py
+Update enviar_alerta_integral.py
+17 hours ago
+Repository files navigation
+README
+gestor-licitaciones-mutuas
+sistema automatizado para rastrear licitaciones en el BOE mutuas y seguros
